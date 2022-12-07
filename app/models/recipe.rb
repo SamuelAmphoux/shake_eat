7115c6c70d9849bea50b958abe32ba977d2f631c
Samuel Amphoux <amphoux.s@hotmail.fr>
2022-12-07 15:50:17 +0100

    bugfix

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
-  has_many :like_users, dependent: :destroy
+  # has_many :like_users, dependent: :destroy
 end
